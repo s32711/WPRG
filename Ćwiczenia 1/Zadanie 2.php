d095ae787eea527460cfcb5a5cadcaa2d774eb66
s32711 <s32711@pjwstk.edu.pl>
2025-03-12 00:32:07 +0100

    Dodaj folder z zadaniami z 1 ćwiczeń

---
+++ Ćwiczenia 1/Zadanie 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie 2</title>
+</head>
+<body>
+<h1>Special characters</h1>
+<h3>Quadratic equation</h3>
+<section style="line-height: 1px">
+    <p>-&#189x<sup>2</sup> + 4x + 10</p>
+    <p>&#8667&#8710 = 16 + 20</p>
+    <p>&#8667&#8730&#8710 = &#8730 26</p>
+    <p>x<sub>1</sub> = 4 + &#8730 26 x<sub>2</sub> = 4 - &#8730 26</p>
+</section>
+<p>Author: Friedrich Bömn, 2022.</p>
+</body>
+</html>
---
+++ Ćwiczenia 1/Zadanie 5.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie 5</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>Produkt</th>
+        <th>Opis</th>
+        <th>Cena</th>
+        <th>Zdjęcie</th>
+    </tr>
+    <tr>
+        <td><a href="https://www.google.com/search?q=telewizor">Telewizor</a></td>
+        <td>Duży telewizor LED o rozmiarze 55 cali.</td>
+        <td>2500zł</td>
+        <td><a href="https://www.google.com/search?q=telewizor"><img src="https://f00.esfr.pl/foto/1/146412858649/45db97949fe59d03785aecf660729299/xiaomi-tv-a-2025-32-l32m8-p2eu-32-led-hd-ready-google-tv-dvb-t2,146412858649_5.webp" width="100" height="100"></a></td>
+    </tr>
+    <tr>
+        <td><a href="https://www.google.com/search?q=laptop">Laptop</a></td>
+        <td>Laptop z procesorem Intel Core i7 i dyskiem SSD.</td>
+        <td>3500zł</td>
+        <td><a href="https://www.google.com/search?q=laptop"><img src="https://gfx3.senetic.com/akeneo-catalog/2/4/3/2/24320c548dbf8a745ef1dc655aad143ec7e0a48e_1626674_5IF_00032_image1.jpg" width="100" height="100"></a></td>
+    </tr>
+    <tr>
+        <td><a href="https://www.google.com/search?q=kamera">Kamera</a></td>
+        <td>Kamera z opcją nagrywania w jakości 4K.</td>
+        <td>1800zł</td>
+        <td><a href="https://www.google.com/search?q=kamera"><img src="https://pl.panasonic.com/wp-content/uploads/2022/09/HC-X2000E_001.jpg" width="100" height="100"></a></td>
+    </tr>
+    <tr>
+        <td><a href="https://www.google.com/search?q=smartfon">Smartfon</a></td>
+        <td>Smartfon z dużym wyświetlaczem i podwójnym aparatem.</td>
+        <td>2000zł</td>
+        <td><a href="https://www.google.com/search?q=smartfon"><img src="https://ecsmedia.pl/c/smartfon-apple-iphone-14-6-128-gb-niebieski-b-iext120245931.jpg" width="100" height="100"></a></td>
+    </tr>
+</table>
+</body>
+</html>
